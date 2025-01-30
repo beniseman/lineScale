b042e25c5bf95f1441b4674b8183c597d32c5637
Ben Iseman <39840968+beniseman@users.noreply.github.com>
2025-01-29 21:56:40 -0500

    Update CerealBox.ino

--- examples/CerealBox/CerealBox.ino
+++ examples/CerealBox/CerealBox.ino
@@ -30,6 +30,38 @@ handleSerialInput();
 linescale->handleTimeout(10);
 }
 
+void newMeasurementCallback() {
+    if (printDataToSerial){
+    Serial.printf(
+      "MAC: %s | "
+      "Zero Mode: %c | "
+      "Force: %.2f %s | "
+      "RelForce: %.2f %s | "
+      "RefZero: %.2f %s | "
+      "ScanRate: %d Hz | "
+      "Battery: %d%% | "
+      "Min: %.2f %s | "
+      "Max: %.2f %s | "
+      "Rate: %d Hz\n",
+      linescale->getMAC().c_str(),  // Print MAC address
+      linescale->ZeroMode(),     // Working mode
+      linescale->Force(),           // Force (measured value)
+      linescale->Unit().c_str(),    // Current unit (e.g., "kN", "kgf", "lbf")
+      linescale->RelativeForce(),   // Relative Force
+      linescale->Unit().c_str(),    // Unit for relative force
+      linescale->ReferenceZero(),   // Reference zero value
+      linescale->Unit().c_str(),    // Unit for reference zero
+      linescale->ScanRate(),        // Scan rate (speed)
+      linescale->BatteryLevel(),    // Battery level
+      linescale->MinForce(),        // Minimum force
+      linescale->Unit().c_str(),    // Unit for min force
+      linescale->MaxForce(),        // Maximum force
+      linescale->Unit().c_str(),    // Unit for max force
+      linescale->NotificationRate() // Notifications per second
+    );
+    }
+}
+
 void handleSerialInput() {
     if (Serial.available()) {
         String command = Serial.readStringUntil('\n'); // Read input
